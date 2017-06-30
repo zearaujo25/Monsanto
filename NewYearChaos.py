bd4942a07a48c6866e552c6bd91ca85d55d63483
zearaujo25 <joselucasdeagc@gmail.com>
2017-06-29 22:55:13 -0300

    Update NewYearChaos.py

--- NewYearChaos.py
+++ NewYearChaos.py
@@ -1,39 +1,18 @@
 def NewYearChaos(queue):
-    lastIndex = len(queue) - 1
-    swaps = 0
-    swaped = False
-    
-    # verifica se as interaoes sao caoticas 
-    for i, v in enumerate(queue):
-        if (v - 1) - i > 2:
-            return "Too chaotic"
-    # aplicando o bubble sort para achar a resposta
-    for i in xrange(0, lastIndex):
-        for j in xrange(0, lastIndex):
-            if queue[j] > queue[j+1]:
-                queue[j], queue[j+1] = queue[j+1], queue[j]
-                swaps += 1
-                swaped = True
-        
-        if swaped:
-            swaped = False
-        else:
-            break
-    return swaps
-def NewYearChaos2(queue):
 	ans=0
 	lastIndex = len(queue) - 1
 	#Passando por todos os elementos do array
 	for i in xrange(0, lastIndex):
 		#checa se o valor do numero nesta posicao menos seu indice e maior que dois. se sim, e muito caotico
 		if (queue[i] - (i+1)) > 2:
-			return "Too chaotic"
-		
+			return "muito caotico para analise"
+			
+		#compara o valor atual com os dois a frente. Se os dois forem menores que o atual, houve dois subornos. se apenas um , houve um suborno. se nenhum entao nao houve nenhum 
 		for j in xrange(i, min(lastIndex+1,i+3)):
 			if queue[i]>q[j]:
-				ans+=1
+				ans+=1	
 	
-	return ans
+	return "de pelo menos "+str(ans)+" subornos"
 
 #LENDO A QUANTIDADE DE CASOS TESTES
 print "Digite a quantidade de casos de Teste: "
@@ -43,8 +22,7 @@ for a0 in xrange(T):
 	print "Digite o tamanho do caso de teste de numero  " + str(a0+1)
 	#LE O TAMANHO DO CASO DE TESTE ANALISADO NAQUELE INSTANTE
 	n = int(raw_input().strip())
-	#LE OS VALORES DAQUELE CASO DE TESTE EM ESPECIFICO, SEPARADOS DOR ESPACO
+	#LE OS VALORES DAQUELE CASO DE TESTE EM ESPECIFICO, SEPARADOS POR ESPACO
 	print "Digite os valores do caso de teste separados por espaco"
 	q = map(int,raw_input().strip().split(' '))
-	#
-	print "Esse caso de teste possui "+str(NewYearChaos2(q))+" subornos"
+	print "Esse caso de teste possui um resultado "+str(NewYearChaos(q))
